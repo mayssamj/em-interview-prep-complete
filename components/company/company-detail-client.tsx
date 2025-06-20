4016e44e46ab4c260e27452f50d4d5eeee94b6fc
Mayssam Sayyadian <6814496+mayssamj@users.noreply.github.com>
2025-06-20 11:10:23 -0700

    Update company-detail-client.tsx

--- components/company/company-detail-client.tsx
+++ components/company/company-detail-client.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 interface CompanyDetailClientProps {
   // Add any props you expect; leaving empty for now
+    company: {
+    name: string;
+    evaluationCriteria: string[];
+    interviewFormat: string;
+    // ...other fields
+  };
+  userStories: UserStory[];
 }
 
 const CompanyDetailClient: React.FC<CompanyDetailClientProps> = () => {
